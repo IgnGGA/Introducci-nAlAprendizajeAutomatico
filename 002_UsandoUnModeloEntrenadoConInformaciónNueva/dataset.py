277c9e9c3bc581a0cd4584ef1bcbfd5cd05603a5
Ignacio <ign.gga@outlook.cl>
2023-08-21 18:14:21 -0400

    haciendo uso del modelo para predecir

--- 002_UsandoUnModeloEntrenadoConInformaciónNueva/dataset.py
+++ 002_UsandoUnModeloEntrenadoConInformaciónNueva/dataset.py
@@ -23,4 +23,15 @@ def llamandoUnModelo():
     cargandoModelo = joblib.load(trabajandoDatos())
     print(f'Nosotros ahora hemos cargado los siguientes parametros:\n{cargandoModelo.params}')
 
-llamandoUnModelo()
+#llamandoUnModelo()
+
+def cargandoUnModeloYPrediciendo(tamanoArnes):
+    import joblib
+    modeloCargado=joblib.load('modeloDataset.pkl')
+    print(f'Ahora nosotros hemos cargado un modelo con los siguientes parametros:\n{modeloCargado.params}')
+    inputs = {'harness_size':[tamanoArnes]}
+    prediccionTamanoArnes = modeloCargado.predict(inputs)[0]
+    return prediccionTamanoArnes
+    
+tamanoAPredecir = cargandoUnModeloYPrediciendo(45)
+print(f'El tamaño de bota predicho es: {tamanoAPredecir}')
